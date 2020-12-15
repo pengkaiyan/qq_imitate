476e2d82ff387b23600798e675b8a213d5f05335
Administrator <2660894933@qq.com>
2020-12-15 11:25:50 +0800

    2020/12/15 11:24

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //将LoginFragment加入，作为首页
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        LoginFragment loginFragment=new LoginFragment();
+        fragmentTransaction.add(R.id.fragment_container,loginFragment);
+        fragmentTransaction.commit();
     }
 }
